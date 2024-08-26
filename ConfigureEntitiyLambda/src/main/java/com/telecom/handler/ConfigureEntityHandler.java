13810d527bdeafb729f9dc446ec42f1d6f28f886
Sergio Genes <genessergio@gmail.com>
2024-08-26 20:07:19 -0300

    Se agrega dynamoDB a la función ConfigureEntityLambda

--- ConfigureEntitiyLambda/src/main/java/com/telecom/handler/ConfigureEntityHandler.java
+++ ConfigureEntitiyLambda/src/main/java/com/telecom/handler/ConfigureEntityHandler.java
@@ -5,23 +5,38 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.telecom.models.PutResponse;
-import com.telecom.models.RecordDefinition;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.telecom.config.DynamoDbConfig;
+import com.telecom.models.CustomObjectFields;
+import com.telecom.models.EloquaAppItem;
+import com.telecom.services.EloquaAppService;
 import com.telecom.services.PutRequestSenderService;
 import com.telecom.services.PutRequestService;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public class ConfigureEntityHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
+    private final EloquaAppService eloquaAppService;
     private final PutRequestService putRequestService = new PutRequestService();
     private final PutRequestSenderService putRequestSenderService = new PutRequestSenderService();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public ConfigureEntityHandler() {
+        // Configura el cliente de DynamoDB y los servicios
+        DynamoDbClient dynamoDbClient = DynamoDbConfig.dynamoDbClient();
+        this.eloquaAppService = new EloquaAppService(dynamoDbClient);
+    }
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
 
         LambdaLogger logger = context.getLogger();
+        Map<String, Object> body;
+
         String contactID = "{{Contact.Id}}"; // Solo ejemplos
         String emailAddress = "{{Contact.Field(C_EmailAddress)}}"; // Solo ejemplos
 
@@ -29,24 +44,34 @@ public class ConfigureEntityHandler implements RequestHandler<APIGatewayProxyReq
         Map<String, String> queryParams = apiGatewayProxyRequestEvent.getQueryStringParameters();
         var bodyString = apiGatewayProxyRequestEvent.getBody();
 
-        //Logger de los parametros receibidos
-        String instance_id = queryParams != null ? queryParams.getOrDefault("instance_id", "default") : "default";
-        String install_id = queryParams != null ? queryParams.getOrDefault("install_id", "default") : "default";
-        String user_name = queryParams != null ? queryParams.getOrDefault("user_name", "default") : "default";
-        String user_id = queryParams != null ? queryParams.getOrDefault("user_id", "default") : "default";
-        String site_name = queryParams != null ? queryParams.getOrDefault("site_name", "default") : "default";
-        String site_id = queryParams != null ? queryParams.getOrDefault("site_id", "default") : "default";
-        String app_id = queryParams != null ? queryParams.getOrDefault("app_id", "default") : "default";
 
-        // Loguear los parámetros
-        logger.log("instance_id: " + instance_id);
-        logger.log("install_id: " + install_id);
-        logger.log("user_name: " + user_name);
-        logger.log("user_id: " + user_id);
-        logger.log("site_name: " + site_name);
-        logger.log("site_id: " + site_id);
-        logger.log("app_id: " + app_id);
-        logger.log(bodyString);
+
+        String instance_id = queryParams.getOrDefault("instance_id", "");
+        if(instance_id == ""){
+            logger.log("Error: instance_id no está definida en los Query Params.");
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(500)
+                    .withBody("{Error : instance_id no está definida en los Query Params}")
+                    .withHeaders(Map.of("Content-Type", "application/json"));
+        }
+
+        EloquaAppItem eloquaAppItem = eloquaAppService.getByInstanceId(instance_id);
+
+        logger.log(eloquaAppItem.toString());
+
+        try{
+            body = objectMapper.readValue(bodyString, Map.class);
+            eloquaAppItem.setCustomObjectId((String) body.get("customObjectId"));
+            eloquaAppItem.setCustomObjectFieldsList( (List<CustomObjectFields>) body.get("customObjectFields"));
+            eloquaAppItem.setTemplate((String) body.get("template"));
+            eloquaAppItem.setState("updated");
+
+            eloquaAppService.updateItem(eloquaAppItem);
+
+        }catch (Exception e){
+            logger.log("Error al actualizar item en la base de datos : " + e.getMessage());
+        }
+
 
         // Acceder a la variable de entorno
         String eloquaToken = System.getenv("ELOQUA_AUTH_TOKEN");
@@ -59,22 +84,12 @@ public class ConfigureEntityHandler implements RequestHandler<APIGatewayProxyReq
                     .withBody("{Error : ELOQUA_AUTH_TOKEN no está definida}")
                     .withHeaders(Map.of("Content-Type", "application/json"))
                     ;
-
-
         }
         // Crear la URL para la solicitud PUT
         String url = "https://secure.p04.eloqua.com/api/cloud/1.0/contents/instances/" + instance_id;
 
         // Crear el objeto RecordDefinitions con los datos necesarios
-        RecordDefinition recordDefinition = new RecordDefinition(contactID, emailAddress);
-
-        // Crear el objeto PutResponse con los detalles de la solicitud PUT
-        PutResponse putResponse = new PutResponse();
-        putResponse.setRecordDefinition(recordDefinition);
-        putResponse.setHeight(256);  // Ejemplo
-        putResponse.setWidth(256);  // Ejemplo
-        putResponse.setEditorImageUrl("https://img04.en25.com/EloquaImages/clients/IngramMicroLATAM/%7B5f315c7b-a380-47af-95aa-140b9a43bd21%7D_32x32.png");  // Ejemplo
-        putResponse.setRequiresConfiguration(false);
+       var putResponse = putRequestService.buildPutRequestBody(contactID, emailAddress);
 
         try {
             // Enviar la solicitud PUT
